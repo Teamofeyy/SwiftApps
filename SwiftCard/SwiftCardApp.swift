3593a36630af337313ac6a39b544c56d7e3f288b
Teamofeyy <timarnd06@gmail.com>
2024-11-01 21:23:05 +0300

    Initial Commit

---
+++ SwiftCard/SwiftCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCardApp.swift
+//  SwiftCard
+//
+//  Created by Тимофей Марченко on 01.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
