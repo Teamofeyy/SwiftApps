e04dc86f688c377fba80c7fc666ff4e2de83a43f
Teamofeyy <timarnd06@gmail.com>
2024-11-02 12:40:34 +0300

    First swift app

--- SwiftCard/ContentView.swift
+++ SwiftCard/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("niagara falls")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15.0)
+                HStack {
+                    Text("Niagara falls")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.fill")
+                        }
+                        Text("(Reviews 361)")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+
+                }
+                Text("Come visit the falls for an unforgettable experience.")
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+                .cornerRadius(15)
+                .shadow(radius: 15)
+            .padding()
         }
-        .padding()
+        
+
     }
 }
 
